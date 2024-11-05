946adbd068aa5bf8518672980a4c8053710a9304
Maxim <incentivedie@gmail.com>
2024-11-05 23:25:45 +0400

    Added custom swipe actions

--- ExpenceTracker/Core/Recent/Views/TransactionRowView.swift
+++ ExpenceTracker/Core/Recent/Views/TransactionRowView.swift
@@ -5,36 +5,42 @@ struct TransactionRowView: View {
     let transaction: Transaction
     
     var body: some View {
-        HStack(spacing: 12) {
-            Text(transaction.title.prefix(1))
-                .font(.title)
-                .fontWeight(.semibold)
-                .foregroundStyle(.white)
-                .frame(width: 45, height: 45)
-                .background(transaction.color.gradient, in: .circle)
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text(transaction.title)
-                    .foregroundStyle(.primary)
+        SwipeAction(cornerRadius: 10, direction: .trailing) {
+            HStack(spacing: 12) {
+                Text(transaction.title.prefix(1))
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 45, height: 45)
+                    .background(transaction.color.gradient, in: .circle)
                 
-                Text(transaction.remarks)
-                    .font(.caption)
-                    .foregroundStyle(.secondary)
-                
-                Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(transaction.title)
+                        .foregroundStyle(.primary)
                     
+                    Text(transaction.remarks)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                    
+                    Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                        
+                }
+                .lineLimit(1)
+                .hSpacing(.leading)
+                
+                Text(currencyFormatter(transaction.amount, digits: 2))
+                    .fontWeight(.semibold)
             }
-            .lineLimit(1)
-            .hSpacing(.leading)
-            
-            Text(currencyFormatter(transaction.amount, digits: 2))
-                .fontWeight(.semibold)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background(.background, in: .rect(cornerRadius: 10))
+        } actions: {
+            Action(tint: .red, icon: "trash") {
+                // TODO: delete
+            }
         }
-        .padding(.horizontal, 15)
-        .padding(.vertical, 10)
-        .background(.background, in: .rect(cornerRadius: 10))
     }
 }
 
